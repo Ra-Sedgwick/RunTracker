65d5ecf5167d410912945441c5578ca473d7b140
Robert Adam Sedgwick <RaSedgwick.gmail.com>
2017-08-14 14:53:49 -0400

    Rebuilt project from older commit. Fixed deployment css, and js issues.

--- src/RunTracker/obj/Release/netcoreapp1.0/PubTmp/Out/wwwroot/js/site.js
+++ src/RunTracker/obj/Release/netcoreapp1.0/PubTmp/Out/wwwroot/js/site.js
@@ -6,7 +6,20 @@
     $("#InUse").bootstrapSwitch();
 
     // Runs/Create: Dropdown date picker
-    $('#datetimepicker').datetimepicker();
+    $('#datetimepicker').datetimepicker({
+        //inline: true,
+        sideBySide: true
+    });
+
+    
+
+    $("#datetimepicker").click(function () {
+        $(".bootstrap-datetimepicker-widget").css("background-color", "#fff");
+        $(".bootstrap-datetimepicker-widget").css("color", "#000");
+    });
+
+    $(".bootstrap-datetimepicker-widget").css("background-color", "#fff");
+    $(".bootstrap-datetimepicker-widget").css("color", "#000");
 
     // Home/Index: Show all runs
     $("#showAllButton").click(function () {
